e94aa2502ae936412eddbc36f5d1cce7fcc2e051
tabeaktr <tabea.ketterer@hs-furtwangen.de>
2021-05-04 22:00:45 +0200

    Aufgabe 1

---
+++ Aufgabe_2.3/script.js
@@ -0,0 +1,41 @@
+"use strict";
+// AUFGABE 2.3
+var Aufgabe1;
+(function (Aufgabe1) {
+    document.addEventListener("DOMContentLoaded", function () {
+        const newRec = document.getElementById("newRec"); //1. Button
+        const recContainer = document.querySelector(".recContainer"); //immer der erste recContainer wird gewählt (Zeichenfläche)
+        // querySelector wählt das erste Objekt hinter der Klammer
+        const reset = document.getElementById("reset"); //2. Button
+        class Rectangle {
+            createRandomRec() {
+                this.width = Math.floor(Math.random() * 100 + 20); //  * 100 + 20   ergibt ganze Zahl 
+                this.height = Math.floor(Math.random() * 100 + 20); // Math.random() ergibt zufällige zahl zwischen 0 und 1
+            }
+            drawRandom(rec) {
+                let x = Math.floor(Math.random() * 700);
+                let y = Math.floor(Math.random() * 300 + 100);
+                let recDiv = document.createElement("div"); // neues div 
+                recDiv.style.width = rec.width + "px"; // erstelltes div zuweisen
+                recDiv.style.height = rec.height + "px";
+                recDiv.style.top = y + "px";
+                recDiv.style.left = x + "px";
+                recDiv.style.position = "absolute";
+                recDiv.style.backgroundColor = "rgba(139, 0, 0, 0.7)";
+                recContainer.appendChild(recDiv); // wird an body angehängt (hinten) | 
+                //append flexibler (appC = auf Obj/El ein von der Klammer)                                ?????????????
+            }
+        }
+        function addNewRec() {
+            let rec = new Rectangle();
+            rec.createRandomRec();
+            rec.drawRandom(rec); //Ausgabe
+        }
+        function clearRecContainer() {
+            recContainer.innerHTML = "";
+        }
+        newRec.addEventListener("click", addNewRec); //Funktion click
+        reset.addEventListener("click", clearRecContainer);
+    });
+})(Aufgabe1 || (Aufgabe1 = {}));
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe_2.3/script.ts
@@ -0,0 +1,48 @@
+// AUFGABE 2.3
+namespace Aufgabe1 {
+    document.addEventListener("DOMContentLoaded", function (): void { // addEvLi sitzt drauf & realisiert was passiert
+        
+        const newRec: HTMLElement = document.getElementById("newRec");   //1. Button
+        const recContainer: HTMLElement = document.querySelector(".recContainer");  //immer der erste recContainer wird gewählt (Zeichenfläche)
+                                                // querySelector wählt das erste Objekt hinter der Klammer
+        const reset: HTMLElement = document.getElementById("reset"); //2. Button
+
+        class Rectangle {
+            width: number;
+            height: number;
+
+            createRandomRec(): void {
+                this.width = Math.floor(Math.random() * 100 + 20);  //  * 100 + 20   ergibt ganze Zahl 
+                this.height = Math.floor(Math.random() * 100 + 20); // Math.random() ergibt zufällige zahl zwischen 0 und 1
+            }
+            
+            drawRandom(rec: Rectangle): void { // Übergabe einer Variablen des Datentyps Rectangle 
+                let x: number = Math.floor(Math.random() * 700);
+                let y: number = Math.floor(Math.random() * 300 + 100); 
+                let recDiv: HTMLDivElement  = document.createElement("div"); // neues div 
+                recDiv.style.width = rec.width + "px"; // erstelltes div zuweisen
+                recDiv.style.height = rec.height + "px";
+                recDiv.style.top = y + "px";
+                recDiv.style.left = x + "px";
+                recDiv.style.position = "absolute";
+                recDiv.style.backgroundColor = "rgba(139, 0, 0, 0.7)";
+                recContainer.appendChild(recDiv); // wird an body angehängt (hinten) | 
+                //append flexibler (appC = auf Obj/El ein von der Klammer)                                ?????????????
+            }
+        }
+
+        function addNewRec(): void {
+            let rec: Rectangle = new Rectangle();
+            rec.createRandomRec();
+            rec.drawRandom(rec); //Ausgabe
+        }
+
+        function clearRecContainer(): void { //recC ALLES wird mit leerem String überschrieben
+            recContainer.innerHTML = "";
+        }
+
+        newRec.addEventListener("click", addNewRec); //Funktion click
+        reset.addEventListener("click", clearRecContainer); 
+        
+    });
+}
