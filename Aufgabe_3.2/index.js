d68ffee04c8ff19d4054cdac136797b28945ad34
tabeaktr <tabea.ketterer@hs-furtwangen.de>
2021-06-04 16:41:42 +0200

    ? wieder eingefügt

--- Aufgabe_3.2/index.js
+++ Aufgabe_3.2/index.js
@@ -12,7 +12,7 @@ var Aufgabe_3_2;
         let url = "https://tabea-ketterer.herokuapp.com";
         url += "/html";
         let query = new URLSearchParams(formData);
-        url = url + query.toString();
+        url = url + "?" + query.toString();
         let response = await fetch(url);
         let responseText = await response.text();
         server.innerHTML = responseText; // füllt die index.html aus
@@ -22,7 +22,7 @@ var Aufgabe_3_2;
         let url = "https://tabea-ketterer.herokuapp.com";
         url += "/json";
         let query = new URLSearchParams(formData);
-        url = url + query.toString();
+        url = url + "?" + query.toString();
         let response = await fetch(url);
         let responseText = await response.json();
         console.log(responseText);
--- Aufgabe_3.2/index.ts
+++ Aufgabe_3.2/index.ts
@@ -13,7 +13,7 @@ namespace Aufgabe_3_2 {
         let url: string = "https://tabea-ketterer.herokuapp.com";
         url += "/html";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url + query.toString();
+        url = url + "?" + query.toString();
         let response: Response = await fetch(url);
         let responseText: string = await response.text();
         server.innerHTML = responseText;                    // füllt die index.html aus
@@ -24,7 +24,7 @@ namespace Aufgabe_3_2 {
         let url: string = "https://tabea-ketterer.herokuapp.com";
         url += "/json";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url + query.toString();
+        url = url + "?" + query.toString();
         let response: Response = await fetch(url);
         let responseText: IResponse = await response.json();
         console.log(responseText);
