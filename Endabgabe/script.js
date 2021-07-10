90290d62fb1c81810bed39cf7e24586796a0d5ed
tabeaktr <tabea.ketterer@hs-furtwangen.de>
2021-07-10 19:57:55 +0200

    aufbau und package.json updated

--- Endabgabe/script.js
+++ Endabgabe/script.js
@@ -1,2 +1,18 @@
 "use strict";
+/*
+1. Array mit Bild-Urls aus DB laden.
+2. Array duplizieren (
+    arrayAusDB;
+    arrayAusDB = arrayAusDB.concat(arrayAusDB);
+
+    wenn nicht:
+        array2 = arrayAusDB;
+)
+
+3. Array shuffeln
+4. Durch Array Loopen:
+    1. Array[i] auf aktuelle index i als id im html setzen (innerhtml)
+   // 2. Array[0] entfernen (https://stackoverflow.com/questions/15292278/how-do-i-remove-an-array-item-in-typescript)
+
+*/
 //# sourceMappingURL=script.js.map
