55149cc6cf0edc27df1e244e04b7c57888829194
tabeaktr <tabea.ketterer@hs-furtwangen.de>
2021-07-10 19:51:44 +0200

    Server.ts set up

---
+++ Endabgabe/server.js
@@ -0,0 +1,51 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Http = require("http");
+const Url = require("url");
+const Mongo = require("mongodb");
+const mongoUrl = "mongodb+srv://User1:hello246@tabea.fzpsx.mongodb.net";
+let timeCollection;
+let urlCollection;
+let port = Number(process.env.PORT);
+if (!port)
+    port = 8100;
+console.log("Starting Server");
+let server = Http.createServer();
+server.addListener("request", handleRequest);
+server.listen(port);
+connectToDatabase(mongoUrl);
+async function connectToDatabase(_url) {
+    let options = { useNewUrlParser: true, useUnifiedTopology: true };
+    let mongoClient = new Mongo.MongoClient(_url, options);
+    await mongoClient.connect();
+    urlCollection = mongoClient.db("Test").collection("URL");
+    timeCollection = mongoClient.db("Test").collection("Times");
+}
+async function handleRequest(_request, _response) {
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    if (_request.url) {
+        let url = Url.parse(_request.url, true);
+        let pathname = url.pathname;
+        if (pathname == "/addUrl") {
+            urlCollection.insertOne(url.query);
+            connectToDatabase(mongoUrl);
+        }
+        if (pathname == "/getUrl") {
+            _response.write(JSON.stringify(await (urlCollection.find().toArray())));
+        }
+        else if (pathname == "/removeUrl") {
+            urlCollection.deleteOne(url.query);
+            connectToDatabase(mongoUrl);
+        }
+        if (pathname == "/addScore") {
+            timeCollection.insertOne(url.query);
+            connectToDatabase(mongoUrl);
+        }
+        if (pathname == "/getScore") {
+            _response.write(JSON.stringify(await (timeCollection.find().toArray())));
+        }
+    }
+    _response.end();
+}
+//# sourceMappingURL=server.js.map
---
+++ Endabgabe/server.ts
@@ -0,0 +1,63 @@
+import * as Http from "http";
+import * as Url from "url";
+import * as Mongo from "mongodb";
+
+const mongoUrl: string = "mongodb+srv://User1:hello246@tabea.fzpsx.mongodb.net";
+
+let timeCollection: Mongo.Collection;
+let urlCollection: Mongo.Collection;
+
+
+let port: number = Number(process.env.PORT);
+if (!port)
+    port = 8100;  
+
+console.log("Starting Server");
+
+let server: Http.Server = Http.createServer();
+server.addListener("request", handleRequest);
+server.listen(port);
+
+connectToDatabase(mongoUrl);
+
+async function connectToDatabase(_url: string): Promise<void> {
+    let options: Mongo.MongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
+    let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
+    await mongoClient.connect();
+
+    urlCollection = mongoClient.db("Test").collection("URL");
+    timeCollection = mongoClient.db("Test").collection("Times");
+}
+
+async function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): Promise<void> {
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+
+
+    if (_request.url) {
+        let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+        let pathname: String | null = url.pathname;
+
+        if (pathname == "/addUrl") {
+            urlCollection.insertOne(url.query);
+            connectToDatabase(mongoUrl);
+        }
+        if (pathname == "/getUrl") {
+            _response.write(JSON.stringify(await (urlCollection.find().toArray())));
+
+        } else if (pathname == "/removeUrl") {
+            urlCollection.deleteOne(url.query);
+            connectToDatabase(mongoUrl);
+        }
+        if (pathname == "/addScore") {
+            timeCollection.insertOne(url.query);
+            connectToDatabase(mongoUrl);
+        }
+        if (pathname == "/getScore") {
+            _response.write(JSON.stringify(await (timeCollection.find().toArray())));
+
+        }
+    }
+    _response.end();
+}
