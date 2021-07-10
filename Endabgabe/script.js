123aff328ca9ca085eae68d8bf0f44c6b85f0823
tabeaktr <tabea.ketterer@hs-furtwangen.de>
2021-07-10 22:19:17 +0200

    :+1:

--- Endabgabe/script.js
+++ Endabgabe/script.js
@@ -15,15 +15,37 @@
    // 2. Array[0] entfernen (https://stackoverflow.com/questions/15292278/how-do-i-remove-an-array-item-in-typescript)
 
 */
-async function clickAbschicken() {
-    // let form: FormData = new FormData(document.forms[0]);
-    // let url: string = "https://tabea-ketterer.herokuapp.com";
-    // let query: URLSearchParams = new URLSearchParams(<any>form);
-    // url = url + "/abschicken" + "?" + query.toString();
-    // await fetch(url);
+async function makeCards() {
     let response = await fetch("https://tabea-ketterer.herokuapp.com/getUrl");
     let listOfUrl = await response.json();
     let dupListOfUrl = listOfUrl;
-    listOfUrl = listOfUrl.concat(dupListOfUrl);
+    listOfUrl = listOfUrl.concat(dupListOfUrl); //langes array mit jedem der 8 Bilder aus der Datenbank doppelt
+    shuffle(listOfUrl);
+    fillBoard(listOfUrl);
+}
+makeCards();
+//Knuth Shuffle Algorithmus (https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array)
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+//tabelle füllen
+function fillBoard(array) {
+    // for (let i: number = 0; array.length; i++) {
+    //    let imageSpot: HTMLElement = document.getElementById("i");
+    //    imageSpot.innerHTML = "<img class='memoryBild' src='" + array[i].url.toString() + "'></img>";
+    // }
+    const cells = document.querySelectorAll("td");
+    cells.forEach(function (cell) {
+        cell.innerHTML = "<img class='Karten' src='" + array[cell.id].url.toString() + "'></img>";
+    });
 }
 //# sourceMappingURL=script.js.map
